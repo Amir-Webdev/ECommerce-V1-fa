5b96d8f1b6bbec319eeac0af9135ebe5af319089
Amir-Webdev <fullstack.dev.amir@gmail.com>
2025-04-15 10:38:24 +0330

    Fixed Upload Image Bug

--- backend/config/s3config.js
+++ backend/config/s3config.js
@@ -1,11 +1,18 @@
 import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./config/.env" });
+
+const endpoint = process.env.ARVAN_ENDPOINT;
+const accessKey = process.env.ARVAN_ACCESS_KEY;
+const secretKey = process.env.ARVAN_SECRET_KEY;
 
 const s3 = new S3Client({
   region: "ir-thr-at1",
-  endpoint: process.env.ARVAN_ENDPOINT,
+  endpoint: endpoint,
   credentials: {
-    accessKeyId: process.env.ARVAN_ACCESS_KEY,
-    secretAccessKey: process.env.ARVAN_SECRET_KEY,
+    accessKeyId: accessKey,
+    secretAccessKey: secretKey,
   },
   forcePathStyle: true,
 });
